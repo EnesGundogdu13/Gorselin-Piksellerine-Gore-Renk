601e5fb14598cadda06f1878a42a46d7ae2de2b2
Enes <enesgndgdu13@gmail.com>
2024-05-19 19:53:09 +0300

    Görselin Piksellerine Göre Renk
    Bu uygulama, kullanıcının seçtiği bir görselin piksellerinin renklerini analiz eder ve tek bir renk verir.

---
+++ Görselin piksellerine göre renk/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Color CreateColor(Bitmap image, int step)
+        {
+            int Red = 0, Green = 0, Blue = 0, pixelCount = 0;
+
+            for (int x = 0; x < image.Width; x += step)
+            {
+                for (int y = 0; y < image.Height; y += step)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+                    Red += pixelColor.R;
+                    Green += pixelColor.G;
+                    Blue += pixelColor.B;
+                    pixelCount++;
+                }
+            }
+
+            int mixedRed = Red / pixelCount;
+            int mixedGreen = Green / pixelCount;
+            int mixedBlue = Blue / pixelCount;
+
+            return Color.FromArgb(mixedRed, mixedGreen, mixedBlue);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap image = new Bitmap(openFileDialog.FileName);
+                Color color = CreateColor(image, 100);
+                label1.BackColor = color;
+            }
+        }
+    }
+}
